fd289254ee6c77e94f0c8935d26e0c9df10a6644
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-02-04 15:56:10 +0530

    BG Updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 style={{display:"flex", marginTop:"50px", justifyContent:"center", color:"white"}}>WELCOME</h1>
+            <h1 className="home-page">WELCOME TO PROFILE APPLICATION </h1>
         </div>
     )
 };
